97bcd508d98cf337b366cb6c1f6647d49d283d23
waylee <969603910@qq.com>
2017-07-25 18:39:29 +0800

    food商品详情页实现

---
+++ src/common/js/date.js
@@ -0,0 +1,30 @@
+/**
+ * Created by liwei on 2017/7/25.
+ */
+function padLeftZero(str) {
+  return ('00'+str).substr(str.length);
+}
+
+export function formatDate(date, fmt) {
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+  }
+  let o = {
+    'M+': date.getMonth() + 1,
+    'd+': date.getDate(),
+    'h+': date.getHours(),
+    'm+': date.getMinutes(),
+    's+': date.getSeconds()
+  };
+  for (let k in o) {
+    if (new RegExp(`(${k})`).test(fmt)) {
+      let str = o[k] + '';
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str));//如果传入的格式是1位，则返回原始值，否则补零。
+    }
+  }
+  return fmt;
+}
+
+function padLeftZero(str) {
+  return ('00' + str).substr(str.length);
+}
--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -17,13 +17,39 @@
           <div class="price">
             <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
           </div>
+          <div class="cartcontrol-wrapper">
+            <cartcontrol :food="food"></cartcontrol>
+          </div>
+          <transition name="fade">
+            <div class="buy" v-show="!food.count || food.count == 0" @click.stop.prevent="addFirst">加入购物车</div>
+          </transition>
         </div>
-        <div class="cartcontrol-wrapper">
-          <cartcontrol :food="food"></cartcontrol>
+        <split v-show="food.info"></split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品介绍</h1>
+          <p class="text">{{food.info}}</p>
         </div>
-        <transition name="fade">
-          <div class="buy" v-show="!food.count || food.count == 0" @click.stop.prevent="addFirst">加入购物车</div>
-        </transition>
+        <split v-show="food.ratings"></split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <ratingselect @selecttype="setType" @content="setOnlycontent" :selectType="selectType" :onlyContent="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+          <div class="ratings-wrapper">
+            <ul v-show="food.ratings && food.ratings.length">
+              <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings" class="rating-item border-1px">
+                <div class="user">
+                  <span class="name">{{rating.username}}</span>
+                  <img class="avatar" width="12" height="12" :src="rating.avatar">
+                </div>
+                <div class="time">{{rating.rateTime | formatDate}}</div>
+                <div class="text">
+                  <span :class="{'icon-thumb_up':rating.rateType == 0,'icon-thumb_down':rating.rateType == 1}"></span>
+                  {{rating.text}}
+                </div>
+              </li>
+            </ul>
+            <div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>
+          </div>
+        </div>
       </div>
     </div>
   </transition>
@@ -31,9 +57,15 @@
 
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll';
-  import cartcontrol from '../cartcontrol/cartcontrol.vue'
+  import cartcontrol from '../cartcontrol/cartcontrol.vue';
+  import split from '../split/split.vue';
+  import ratingselect from '../ratingselect/ratingselect.vue';
   import Vue from 'vue';
+  import {formatDate} from '../../common/js/date'
 
+  const POSITIVE = 0;
+  const NEGATIVE = 1;
+  const ALL = 2;
   export default {
     props:{
       food:{
@@ -42,12 +74,21 @@
     },
     data(){
       return{
-        showFlag:false
+        showFlag:false,
+        selectType:ALL,
+        onlyContent:true,
+        desc:{
+          all:'全部',
+          positive:'推荐',
+          negative:'吐槽'
+        }
       }
     },
     methods:{
       show() {
         this.showFlag = true;
+        this.selectType = ALL;
+        this.onlyContent = false;
         this.$nextTick(() => {
           if(!this.scroll) {
             this.scroll = new BScroll(this.$refs.food,{
@@ -67,15 +108,48 @@
         }
         this.$root.eventHub.$emit('cartAdd', event.target);
         Vue.set(this.food,'count',1)
+      },
+      setType(type) {
+        this.selectType = type;
+        //手动刷新better-scroll重新计算页面高度
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      },
+      setOnlycontent() {
+        this.onlyContent = !this.onlyContent;
+        //手动刷新better-scroll重新计算页面高度
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      },
+      needShow(type,text) {
+        if(this.onlyContent && !text) {
+          return false;
+        }
+        if(this.selectType == ALL) {
+          return true;
+        } else {
+          return type == this.selectType;
+        }
+      }
+    },
+    filters:{
+      formatDate(time) {
+        let date = new Date(time);
+        return formatDate(date,'yyyy-MM-dd hh:mm');
       }
     },
     components:{
-      cartcontrol
+      cartcontrol,
+      split,
+      ratingselect
     }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
   .food
     position fixed
     left 0
@@ -111,6 +185,7 @@
 
 
     .content
+      position relative
       padding 18px
       .title
         font-weight 700
@@ -139,25 +214,91 @@
           text-decoration line-through
           font-size 10px
           color rgb(147,153,159)
-    .cartcontrol-wrapper
-      position absolute
-      right 12px
-      bottom 12px
-    .buy
-      position absolute
-      right 18px
-      bottom 18px
-      z-index 10
-      height 24px
-      line-height 24px
-      padding 0 12px
-      box-sizing border-box
-      font-size 10px
-      border-radius 12px
-      color #fff
-      background rgb(0,160,220)
-      transition all .2s
-      opacity 1
-      &.fade-enter-active,&.fade-leave-active
-        opacity 0
+      .cartcontrol-wrapper
+        position absolute
+        right 12px
+        bottom 12px
+      .buy
+        position absolute
+        right 18px
+        bottom 18px
+        z-index 10
+        height 24px
+        line-height 24px
+        padding 0 12px
+        box-sizing border-box
+        font-size 10px
+        border-radius 12px
+        color #fff
+        background rgb(0,160,220)
+        transition all .2s
+        opacity 1
+        &.fade-enter-active,&.fade-leave-active
+          opacity 0
+    .info
+      padding 18px
+      .title
+        line-height 14px
+        margin-bottom 6px
+        font-size 14px
+        color rgb(7,17,27)
+      .text
+        line-height 24px
+        padding 0 8px
+        font-size 12px
+        color rgb(77,85,93)
+    .rating
+      padding-top 18px
+      .title
+        line-height 14px
+        margin-left 18px
+        font-size 14px
+        color rgb(7,17,27)
+      .ratings-wrapper
+        padding 0 18px
+        .rating-item
+          position relative
+          padding 16px 0
+          border-1px(rgba(7,17,27,.2))
+          .user
+            position absolute
+            right 0
+            top 16px
+            line-height 12px
+            font-size 0
+            .name
+              display inline-block
+              vertical-align top
+              font-size 10px
+              margin-right 6px
+              color rgb(147,153,159)
+            .avatar
+              border-radius 50%
+
+
+          .time
+            margin-bottom 6px
+            line-height 12px
+            font-size 10px
+            color rgb(143,153,159)
+          .text
+            line-height 16px
+            font-size 12px
+            color rgb(7,17,27)
+            span
+              margin-right 4px
+              line-height 16px
+              vertical-align top
+              font-size 12px
+            .icon-thumb_up
+              color rgb(0,160,220)
+            .icon-thumb_down
+              color rgb(147,153,159)
+
+
+        .no-rating
+          padding 16px 0
+          font-size 12px
+          color rgb(147,153,159)
+
 </style>
